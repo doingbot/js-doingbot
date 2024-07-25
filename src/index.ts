83e67568ee19b378824bd6bc6f3b885ab5df4899
deframework <deframework@outlook.com>
2024-07-25 16:51:51 +0800

    都搞定了

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *	models
+ */
+export * from "./model/ParseModel";
+
+
+/**
+ *	services
+ */
+export * from "./services/DouDownloader";
+export * from "./services/DouParser";
+
+
+/**
+ * 	utils
+ */
+export * from "./utils/DouNetworkUtil";
+export * from "./utils/DouImageUtil";
+export * from "./utils/DouNetworkUtil";
--- src/services/DouParser.ts
+++ src/services/DouParser.ts
@@ -7,6 +7,9 @@ import { ParseResult } from "../model/ParseModel";
 //export const userAgent = 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36';
 export const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36';
 
+/**
+ * 	@class
+ */
 export class DouParser
 {
 	virtualConsole = new VirtualConsole();
